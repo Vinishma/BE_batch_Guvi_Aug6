8de125f92334cd26f2da8e7a51953dbb5e164de6
Vinishma <142059212+Vinishma@users.noreply.github.com>
2023-09-08 11:36:04 +0400

    assignment

---
+++ Zen_id_vinishma_solutions/Palindrome.py
@@ -0,0 +1,5 @@
+string=input(("Enter a string:"))
+if(string==string[::-1]):              # reversing the string
+        print("The string is a palindrome")
+else:
+        print("The string isn't a palindrome")
